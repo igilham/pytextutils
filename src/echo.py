9082f923fd62bc7de585c6239b7da58c9b5ad7b1
Ian Gilham <ian.gilham@gmail.com>
2011-01-26 19:49:34 +0000

    finally got the simple echo program to work correctly

--- src/echo.py
+++ src/echo.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import getopt
 
-#parser = optparse.OptionParser()
-#parser.add_options('-n' '--omit-newline', dest="omit_newline", 
-#					action="store-true", help='omit trailing newline',
-#					default=False)
-
+newline = '\n'
+space = ' '
+omitnewline = False
 
 def main():
+	global omitnewline
 	options, args = getopt.getopt(sys.argv[1:], 'n')
+	sys.stdout.write(space.join(args))
 	
-	newline = '\n'
-	if '-n' in options:
-		newline = ''
-	for arg in args:
-		print(arg, end=newline)
+	for opt in options:
+		if '-n' in opt:
+			omitnewline = True
+			break
+	if not omitnewline:
+		sys.stdout.write(newline)
 
 
 if __name__ == "__main__":
